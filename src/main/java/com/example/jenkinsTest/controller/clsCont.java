5e33a37e63ffdf6358858648b1b1e59963c12886
fatihstudent41@gmail.com <93310233+achilles41@users.noreply.github.com>
2023-05-21 19:12:05 +0200

    first jenkins

---
+++ src/main/java/com/example/jenkinsTest/controller/clsCont.java
@@ -0,0 +1,33 @@
+package com.example.jenkinsTest.controller;
+
+import java.io.IOException;
+
+import java.sql.SQLException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class clsCont {
+
+	@GetMapping("/test")
+	public ResponseEntity<?> test()  
+	{
+		try {
+
+		return new ResponseEntity<String>("Done",HttpStatus.OK);
+		}
+	 
+		catch (Exception e) {
+			return new ResponseEntity<String>("Error",HttpStatus.INTERNAL_SERVER_ERROR); 
+		}
+		
+		
+	}
+	
+}
